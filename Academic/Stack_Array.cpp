011acd41c1e5754562423cadcfd41f531a7ab973
Ibrahim H <mohammad.ihakash@gmail.com>
2022-08-28 20:38:06 +0600

    updated

---
+++ Academic/Stack_Array.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+#include <bits/stdc++.h>
+#define FASTio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long int
+#define cin(x) cin >> x
+#define cin2(x, y) cin >> x >> y
+#define cin3(x, y, z) cin >> x >> y >> z
+#define cin4(x, y, z, k) cin >> x >> y >> z >> k
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
--- Academic/Stack_LinkedList.cpp
+++ Academic/Stack_LinkedList.cpp
@@ -1,13 +1,4 @@
-//#include <iostream>
-#include <bits/stdc++.h>
-#define FASTio                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
-#define ll long long int
-#define cin(x) cin >> x
-#define cin2(x, y) cin >> x >> y
-#define cin3(x, y, z) cin >> x >> y >> z
-#define cin4(x, y, z, k) cin >> x >> y >> z >> k
+#include <iostream>
 using namespace std;
 
 class Stack
