3c8df79ec4709c17ffa77f67f86d6ffe7ded0855
Ibrahim Hossain <ibuhossain3@gmail.com>
2022-11-26 19:12:45 +0600

    update: DFS added

---
+++ Academic/DFS.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void DFS(int start, int visited[], vector<int> adj[])
+{
+    visited[start] = 1;
+    cout << start << " ";
+    for (auto it : adj[start])
+    {
+        if (!visited[it])
+        {
+            DFS(it, visited, adj);
+        }
+    }
+}
+
+int main()
+{
+    int edge;
+    int start;
+    int totalNode;
+    vector<int> adj[totalNode + 2];
+    int visited[totalNode + 2] = {0};
+    // input total node number and total connections
+    cin >> totalNode >> edge;
+    // input connections
+    for (int i = 1; i <= edge; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    // input starting node
+    cin >> start;
+    DFS(start, visited, adj);
+}
